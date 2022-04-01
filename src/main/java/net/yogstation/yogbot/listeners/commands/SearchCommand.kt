8e496c3a2c7afa649523342e225b66c1c8a5f21c
adamsong <adamsong@users.noreply.github.com>
2022-04-01 18:13:51 -0500

    Add command for the lazy (#35)

---
+++ src/main/java/net/yogstation/yogbot/listeners/commands/SearchCommand.kt
@@ -0,0 +1,26 @@
+package net.yogstation.yogbot.listeners.commands
+
+import discord4j.core.event.domain.message.MessageCreateEvent
+import net.yogstation.yogbot.config.DiscordConfig
+import net.yogstation.yogbot.util.DiscordUtil
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+@Component
+class SearchCommand(
+	discordConfig: DiscordConfig
+) : TextCommand(discordConfig) {
+	override val name = "search"
+
+	override val description = "Searches the specified github for the string"
+
+	override fun doCommand(event: MessageCreateEvent): Mono<*> {
+		val args: List<String> = event.message.content.split(" ", limit = 3)
+		if(args.size < 3) return DiscordUtil.reply(event, "Usage: `$args[0] <repo> <text>`")
+		val repo = URLEncoder.encode(args[1], StandardCharsets.UTF_8)
+		val query = URLEncoder.encode(args[2], StandardCharsets.UTF_8)
+		return DiscordUtil.reply(event, "https://github.com/yogstation13/$repo/search?q=$query")
+	}
+}
