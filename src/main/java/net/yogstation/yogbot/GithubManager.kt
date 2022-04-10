ea976b162ea08f9d191e1f6e458e196395120ff4
adamsong <adamsong@users.noreply.github.com>
2022-04-10 07:46:35 -0500

    Fixes changelog generation

--- src/main/java/net/yogstation/yogbot/GithubManager.kt
+++ src/main/java/net/yogstation/yogbot/GithubManager.kt
@@ -42,7 +42,7 @@ class GithubManager(
 		changelogFile.append("\"\ndelete-after: true \nchanges: \n")
 
 		for (change in changelog.entries) {
-			val body = change.body.replace("\"", "\\\\\"").replace("<", "")
+			val body = change.body.replace("\"", "\\\"").replace("<", "")
 			changelogFile.append("  - ${change.type}: \"$body\"\n")
 		}
 
