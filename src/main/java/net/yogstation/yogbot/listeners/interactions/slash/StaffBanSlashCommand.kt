85be7f09d7c3a01aedf50a120794bd4149821551
adamsong <adamsong@users.noreply.github.com>
2022-04-26 00:47:50 -0500

    [s] Fix oversight in slash command permissions

--- src/main/java/net/yogstation/yogbot/listeners/interactions/slash/StaffBanSlashCommand.kt
+++ src/main/java/net/yogstation/yogbot/listeners/interactions/slash/StaffBanSlashCommand.kt
@@ -1,17 +1,19 @@
 package net.yogstation.yogbot.listeners.interactions.slash
 
 import discord4j.common.util.Snowflake
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent
 import discord4j.core.`object`.command.ApplicationCommandInteractionOption
 import discord4j.core.`object`.command.ApplicationCommandInteractionOptionValue
-import discord4j.core.event.domain.interaction.ChatInputInteractionEvent
 import net.yogstation.yogbot.listeners.interactions.ISlashCommand
+import net.yogstation.yogbot.permissions.PermissionsManager
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 import net.yogstation.yogbot.listeners.interactions.StaffBanCommand as SBCommand
 
 @Component
 class StaffBanSlashCommand(
-	private val sbCommand: SBCommand
+	private val sbCommand: SBCommand,
+	private val permissions: PermissionsManager
 ): ISlashCommand {
 	override val name = "staffban"
 
@@ -20,7 +22,9 @@ class StaffBanSlashCommand(
 			.map(ApplicationCommandInteractionOptionValue::asSnowflake).orElse(Snowflake.of(0))
 		if(userid.asLong() == 0L) event.reply().withEphemeral(true).withContent("Failed to find person")
 
-		return event.interaction.guild.flatMap { guild ->
+		return if (!permissions.hasPermission(event.interaction.member.orElse(null), "staffban")) event.reply()
+			.withEphemeral(true).withContent("You do not have permission to run that command")
+		else event.interaction.guild.flatMap { guild ->
 			guild.getMemberById(userid).flatMap { member ->
 				sbCommand.staffBan(member, event)
 			}
