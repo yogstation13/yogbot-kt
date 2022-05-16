f74d90ecc86eadca8f4311522bb257102d9d6589
adamsong <adamsong@users.noreply.github.com>
2022-05-16 15:11:36 -0400

    Improves admin notification of tickets (#54)
    * Improves admin notification of tickets
    
    * Fixes linter
    
    * Makes it work
    
    * Fixed detekt error

---
+++ src/main/java/net/yogstation/yogbot/http/byond/UnclaimedTicketEndpoint.kt
@@ -0,0 +1,39 @@
+package net.yogstation.yogbot.http.byond
+
+import discord4j.common.util.Snowflake
+import discord4j.core.GatewayDiscordClient
+import net.yogstation.yogbot.config.ByondConfig
+import net.yogstation.yogbot.config.DiscordChannelsConfig
+import net.yogstation.yogbot.config.DiscordConfig
+import net.yogstation.yogbot.http.ByondEndpoint
+import net.yogstation.yogbot.http.byond.payloads.UnclaimedTicketDTO
+import net.yogstation.yogbot.util.HttpUtil
+import org.springframework.http.HttpEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
+@RestController
+class UnclaimedTicketEndpoint(
+		private val client: GatewayDiscordClient,
+		private val discordConfig: DiscordConfig,
+		private val discordChannelsConfig: DiscordChannelsConfig,
+		byondConfig: ByondConfig
+): ByondEndpoint(byondConfig) {
+    // Function to take an unclaimed ticket DTO and send the ticket to the admin channel
+    @PostMapping("/byond/ticket_unclaimed")
+    fun unclaimedTicket(@RequestBody unclaimedTicketDTO: UnclaimedTicketDTO): Mono<HttpEntity<String>> {
+		val keyError = validateKey(unclaimedTicketDTO.key)
+		if (keyError != null)
+			return keyError
+        // Send the ticket to the admin channel
+		val message =
+				"@here Unclaimed Ticket #${unclaimedTicketDTO.id} (${unclaimedTicketDTO.ckey}): ${unclaimedTicketDTO.message}"
+		return client.getGuildById(Snowflake.of(discordConfig.mainGuildID)).flatMap { guild ->
+			guild.getChannelById(Snowflake.of(discordChannelsConfig.channelAdmin)).flatMap { channel ->
+				channel.restChannel.createMessage(message)
+			}
+		}.then(HttpUtil.ok("Success"))
+    }
+}
---
+++ src/main/java/net/yogstation/yogbot/http/byond/payloads/UnclaimedTicketDTO.kt
@@ -0,0 +1,10 @@
+package net.yogstation.yogbot.http.byond.payloads
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+class UnclaimedTicketDTO (
+	@JsonProperty("ckey") val ckey: String,
+	@JsonProperty("message") val message: String,
+	@JsonProperty("id") val id: Int,
+	@JsonProperty("key") val key: String
+)
