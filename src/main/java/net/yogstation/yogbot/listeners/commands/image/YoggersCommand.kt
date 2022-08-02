1df136621bc36c2f39f20eb56a72bc697d82d0bb
LoliconSlayer <54045237+LoliconSlayer@users.noreply.github.com>
2022-08-02 15:55:36 -0400

    Update src/main/java/net/yogstation/yogbot/listeners/commands/image/YoggersCommand.kt
    Co-authored-by: Ashleigh Carr <ashcorr20@gmail.com>

--- src/main/java/net/yogstation/yogbot/listeners/commands/image/YoggersCommand.kt
+++ src/main/java/net/yogstation/yogbot/listeners/commands/image/YoggersCommand.kt
@@ -89,7 +89,6 @@ class YoggersCommand(discordConfig: DiscordConfig, random: Random) : ImageComman
 		"https://cdn.discordapp.com/attachments/734475284446707753/976966811268567111/unknown.png", // Bloodsucker spam
 		"https://cdn.discordapp.com/attachments/734475284446707753/978415638546616390/unknown.png", // The end is neigh
 		"https://cdn.discordapp.com/attachments/734475284446707753/982434340103405598/unknown.png", // Grill with the boys
-		//
 		"https://cdn.discordapp.com/attachments/734475284446707753/989004233808445440/unknown.png" // Dominance over Nightmare
 	)
 
