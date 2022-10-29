a88ffa5b834ebdd7271d97f3a5f064b50d5f3a51
LoliconSlayer <54045237+LoliconSlayer@users.noreply.github.com>
2022-10-29 14:42:01 -0700

    Another update (#81)
    * Final purge
    
    * Update CouncilCommand.kt
    
    * Update YoggersCommand.kt
    
    * Update MojaCommand.kt
    
    * Update MentorCommand.kt

--- src/main/java/net/yogstation/yogbot/listeners/commands/image/CouncilCommand.kt
+++ src/main/java/net/yogstation/yogbot/listeners/commands/image/CouncilCommand.kt
@@ -14,7 +14,10 @@ class CouncilCommand(discordConfig: DiscordConfig, random: Random) : ImageComman
 			"https://cdn.discordapp.com/attachments/734475284446707753/804697809323556864/unknown.png", // Get a sense of humor
 			"https://cdn.discordapp.com/attachments/734475284446707753/804192496322084864/ban.png", // Banned by public vote
 			"https://cdn.discordapp.com/attachments/734475284446707753/800864882870059028/image0.png", // Council don't know hotkeys
-			"https://cdn.discordapp.com/attachments/734475284446707753/959203191478681661/unknown.png" // Xantam moderator app
+			"https://cdn.discordapp.com/attachments/734475284446707753/959203191478681661/unknown.png", // Xantam moderator app
+			"https://cdn.discordapp.com/attachments/734475284446707753/1004911936124764303/Screenshot_2022-08-04_183955.png", // You expect adam to read?
+			"https://cdn.discordapp.com/attachments/734475284446707753/1028837256481493012/unknown.png", // YOU'RE COUNCIL. SO ARE YOU
+			"https://cdn.discordapp.com/attachments/734475284446707753/1031688697533435904/IMG_9748.png" // Adam has thoughts?
 		)
 	override val title = "Council Image"
 	override val name = "council"
