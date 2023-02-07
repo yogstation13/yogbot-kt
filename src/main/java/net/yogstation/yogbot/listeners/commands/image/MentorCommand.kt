d831c6f4a078d3599ab5bae9b71582a1ba65e4ba
Thomas <36747151+ReddicusDragon@users.noreply.github.com>
2023-02-07 09:02:38 -0500

    Update MentorCommand.kt

--- src/main/java/net/yogstation/yogbot/listeners/commands/image/MentorCommand.kt
+++ src/main/java/net/yogstation/yogbot/listeners/commands/image/MentorCommand.kt
@@ -115,7 +115,8 @@ class MentorCommand(
 		//June Update
 		"https://cdn.discordapp.com/attachments/734475284446707753/980826387998973953/preternis.png", //How recharge as preternis
 		"https://cdn.discordapp.com/attachments/734475284446707753/981416123532730428/loophole.png", //Loopless Engine
-		"https://cdn.discordapp.com/attachments/734475284446707753/1017396538697338940/unknown.png" // Self mhelp
+		"https://cdn.discordapp.com/attachments/734475284446707753/1017396538697338940/unknown.png", // Self mhelp
+		"https://cdn.discordapp.com/attachments/134720091576205312/1072515691749572679/image-137.png" // Sarah and the Hyper Torus
 	)
 	override val title = "Mentor Image"
 	override val color = 0x259E80
