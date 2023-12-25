1452745a122bdfe34eb3c54e8f0042d951b43867
Byemoh <baiomurang@gmail.com>
2023-12-24 20:27:58 -0600

    Update PublicChannel.kt

--- src/main/java/net/yogstation/yogbot/listeners/channel/PublicChannel.kt
+++ src/main/java/net/yogstation/yogbot/listeners/channel/PublicChannel.kt
@@ -13,8 +13,8 @@ class PublicChannel(channelsConfig: DiscordChannelsConfig) : AbstractChannel(cha
 	override fun handle(event: MessageCreateEvent): Mono<*> {
 		val message = event.message
 		var content = message.getContent()
-		if (!content.contains("hohoho", ignoreCase = true) && !content.contains("hohoho", ignoreCase = true)) {
-			message.delete("You must be festive! Include "hohoho" or "ho ho ho" in your message for the christmas season!")
+		return if (!content.contains("hohoho", ignoreCase = true) && !content.contains("hohoho", ignoreCase = true)) {
+			message.delete("You must be festive! Include \"hohoho\" or \"ho ho ho\" in your message for the christmas season!")
 		}
 	}
 }
