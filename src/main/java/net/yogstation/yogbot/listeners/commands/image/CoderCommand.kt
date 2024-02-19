42feff1bf32f09b3aa473db3f0312cadffaaa6e5
TheRandomHobo <116853115+Therandomhoboo@users.noreply.github.com>
2024-02-19 01:27:45 +0000

    I AM A CODER I AM A CODER I AM A CODER  (#134)
    * I AM A CODER I AM A CODER
    
    * Update CoderCommand.kt
    
    * Update CoderCommand.kt

--- src/main/java/net/yogstation/yogbot/listeners/commands/image/CoderCommand.kt
+++ src/main/java/net/yogstation/yogbot/listeners/commands/image/CoderCommand.kt
@@ -71,7 +71,8 @@ class CoderCommand(discordConfig: DiscordConfig, random: Random) : ImageCommand(
 		"https://cdn.discordapp.com/attachments/734475284446707753/1099496558870921217/image.png", // Can't call it an ided
 		"https://cdn.discordapp.com/attachments/734475284446707753/1103234273735090196/image.png", // Just get playtime
 		"https://cdn.discordapp.com/attachments/408008557993132035/1161765671085023403/image.png", // I'm normal I swear
-		"https://cdn.discordapp.com/attachments/892167119154872391/1161764164788830328/and_if_we_help.png" // Helping out
+		"https://cdn.discordapp.com/attachments/892167119154872391/1161764164788830328/and_if_we_help.png", // Helping out
+		"https://cdn.discordapp.com/attachments/1094309532341117061/1179647880970129408/i_am_a_coder.png", // I AM A CODER
 	)
 
 	override val title = "Coder Image"
