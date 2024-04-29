260b1a24a4e578229e51d4c7787a8628b8cad2a5
LoliconSlayer <54045237+LoliconSlayer@users.noreply.github.com>
2024-04-29 14:35:06 -0400

    Update MessageListener.kt (#140)

--- src/main/java/net/yogstation/yogbot/listeners/MessageListener.kt
+++ src/main/java/net/yogstation/yogbot/listeners/MessageListener.kt
@@ -28,7 +28,7 @@ class MessageListener(
 		val content: String = event.message.content
 		if (event.message.author.isEmpty) return responses
 
-		if (content.contains("snail", ignoreCase = true) && content.contains("when", ignoreCase = true))
+		if (content.contains("new pr:", ignoreCase = true))
 			responses = responses.and(DiscordUtil.reply(event, "When you code it"))
 
 		val jesterRole = Snowflake.of(discordConfig.jesterRole)
